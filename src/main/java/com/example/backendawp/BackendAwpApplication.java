3499a9bfc3bc70198a2e8451d4d809c16baffb36
jdjurisic <jovica.djurisic@gmail.com>
2021-07-16 18:56:49 +0200

    Initial commit

---
+++ src/main/java/com/example/backendawp/BackendAwpApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendawp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAwpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAwpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendawp/BackendAwpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendawp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAwpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
