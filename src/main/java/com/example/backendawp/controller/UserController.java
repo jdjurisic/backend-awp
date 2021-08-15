136664ef2ff2c5ba4bd15125e48f14b77697167c
jovicadj <jdjurisic17@raf.rs>
2021-08-15 22:10:32 +0200

    User controller created.

--- src/main/java/com/example/backendawp/controller/UserController.java
+++ src/main/java/com/example/backendawp/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.example.backendawp.controller;
 
+import com.example.backendawp.model.User;
 import com.example.backendawp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("/users")
 @CrossOrigin
 public class UserController {
 
@@ -17,5 +23,43 @@ public class UserController {
         this.service = service;
     }
 
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> save(@Valid @RequestBody User user){
+        return new ResponseEntity<>(service.save(user), HttpStatus.CREATED);
+    }
+
+
+    @PutMapping ("/{userId}")
+    public ResponseEntity<User> update( @PathVariable Long userId, @Valid @RequestBody User user){
+        if(service.findById(userId)==null)
+            return null;
+        return new ResponseEntity<>(service.update(userId, user), HttpStatus.CREATED);
+    }
+
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> deleteById(@PathVariable Long userId){
+        service.deleteById(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Optional<User>> findById( @PathVariable Long userId){
+        return new ResponseEntity<Optional<User>>(service.findById(userId), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/getByUsername/{username}")
+    public ResponseEntity<User> findByUsername( @PathVariable String username){
+        return new ResponseEntity<>(service.findByUsername(username), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/all")
+    public List<User> findAll(){
+        return service.findAll();
+    }
 
 }
